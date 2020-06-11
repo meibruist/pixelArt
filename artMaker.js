f9459af0cd040aa713b60f6b35bd1cc2a506089f
meibruist <msoc14@gmail.com>
2020-06-10 18:28:39 -0700

    color palatte created

--- artMaker.js
+++ artMaker.js
@@ -3,25 +3,43 @@
 // This is crucial, because our div (the "canvas") might not exist if we do not wait.
 
 window.onload = function(){
-  let container = document.querySelector('#canvas');
+  
+  // create container for the pixel box
+  let containerCanvas = document.querySelector('#canvas');
 
-  for(let i = 0; i < 400; i++) {
+  for(let i = 0; i < 400; i++) {  // if i want more boxes / pixels, change it here
     let box = document.createElement('span');
     box.style.border = '1px solid black';
     box.style.height = '8px';
     box.style.width = '8px';
 
-    container.appendChild(box);
+    containerCanvas.appendChild(box);
   }
-  document.body.appendChild(container);    
+  document.body.appendChild(containerCanvas);    
 
-  let clickBox = document.querySelectorAll('span');
+  // event listener for to change color to red
+  let clickBox = document.querySelectorAll('span');  
   
-  for(let pixelBox of clickBox) {
+  for(let pixelBox of clickBox) {             // more flexible for loop so it doesn't matter the number of pixels
   pixelBox.addEventListener('click', function(event) {
     pixelBox.style.backgroundColor = 'red'
   })
   }
+
+  // color palatte
+  let containerPalatte = document.querySelector('#palatte');
+  let colorChoices = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+
+  for(let color of colorChoices) {
+    let boxPalatte = document.createElement('span');
+    boxPalatte.style.border = '2px solid black';
+    boxPalatte.style.backgroundColor = color
+    containerPalatte.appendChild(boxPalatte);
+  }
+ document.body.prepend(containerPalatte);
+ 
+
+
 
 }
   
